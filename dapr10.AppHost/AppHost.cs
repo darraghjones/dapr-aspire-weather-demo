e4e3b3bc3793a34284ca06aebacf824075cb4b21
Darragh Jones <darragh.jones@geniussports.com>
2025-12-09 23:55:35 +0000

    add config.yaml

--- dapr10.AppHost/AppHost.cs
+++ dapr10.AppHost/AppHost.cs
@@ -7,15 +7,15 @@ DaprSidecarOptions CreateSidecarOptions(int metricsPort) => new()
     LogLevel = "Debug",
     EnableApiLogging = true,
     MetricsPort = metricsPort,
-    //Config = ""
+    Config = "../config.yaml"
 };
 
 var apiService = builder.AddProject<Projects.dapr10_ApiService>("apiservice")
-    .WithDaprSidecar(CreateSidecarOptions(9001))
+    .WithDaprSidecar(CreateSidecarOptions(9091))
     .WithHttpHealthCheck("/health");
 
 builder.AddProject<Projects.dapr10_Web>("webfrontend")
-    .WithDaprSidecar(CreateSidecarOptions(9002))
+    .WithDaprSidecar(CreateSidecarOptions(9092))
     //.WithExternalHttpEndpoints()
     .WithHttpHealthCheck("/health")
     .WithReference(apiService)
